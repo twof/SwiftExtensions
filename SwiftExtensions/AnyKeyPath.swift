25f03abf85bb1b280b1762d0dd2a389263d6a6d9
twof <fabiobean2@gmail.com>
2017-11-02 16:13:26 -0700

    Documentation and UIViewController extension

--- SwiftExtensions/AnyKeyPath.swift
+++ SwiftExtensions/AnyKeyPath.swift
@@ -1,20 +1,15 @@
-//
-//  KeyPath.swift
-//  SwiftExtensions
-//
-//  Created by New User on 11/1/17.
-//  Copyright © 2017 twof. All rights reserved.
-//
-
 import Foundation
 
 extension AnyKeyPath {
+    
+    /// Gets the key of a KeyPath as a string. Good for debugging
     var key: String? {
         let splitList = String(describing: self).split(whereSeparator: {">,.".contains($0)})
         guard splitList.count > 3 else { return nil }
         return String(splitList[2])
     }
     
+    /// Gets the value of a KeyPath as a string. Good for debugging
     var value: String? {
         guard let value = (String(describing: self).split{">,.".contains($0)}.last) else { return nil }
         return String(describing: value)
---
+++ SwiftExtensions/UIViewController.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+extension UIViewController {
+    func setBackButtonTitle(title: String) {
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: title, style: .plain, target: nil, action: nil)
+    }
+    
+    /// Fades the title of the view to the nav bar as the user scrolls past the header view
+    func titleToNavBar(scrollView: UIScrollView, headerView: UIView, title: String) {
+        let scrollDist = -1 * (scrollView.contentOffset.y + scrollView.contentInset.top)
+        guard let navBar = navigationController?.navigationBar else {return}
+        guard let currentTitleColor = navBar.titleTextAttributes?[NSForegroundColorAttributeName] as? UIColor
+            else {return}
+        guard let prevControllers = self.navigationController?.viewControllers,
+            prevControllers.count > 0
+            else {return}
+        let previousViewController = prevControllers[0]
+        
+        let fadeSpeed: CGFloat = 2
+        
+        // Back button fade out math
+        let headerFirstQuarter = (headerView.frame.height * -1)/4...0
+        
+        // Title fade in math
+        let headerSecondQuarter = (headerView.frame.height * -1)/2..<(headerFirstQuarter.lowerBound)
+        let isTopAnchorInSecondQuarter = headerSecondQuarter.contains(scrollDist)
+        let absSecondUpperBound = abs(scrollDist-headerSecondQuarter.upperBound)
+        let absSecondLowerBound = abs(headerSecondQuarter.lowerBound)
+        let titleAlphaToAnchorRatio = fadeSpeed*(absSecondUpperBound/absSecondLowerBound)
+        let titleAlphaComponent = (isTopAnchorInSecondQuarter) ? titleAlphaToAnchorRatio : 1.0
+        let titleColorWithAlpha = currentTitleColor.withAlphaComponent
+        let scalingAlphaAttributes = [NSForegroundColorAttributeName: titleColorWithAlpha(titleAlphaComponent)]
+        let zeroAlphaAttributes = [NSForegroundColorAttributeName: titleColorWithAlpha(0)]
+        let oneAlphaAttributes = [NSForegroundColorAttributeName: titleColorWithAlpha(1.0)]
+        
+        switch scrollDist {
+        case headerFirstQuarter:
+            navBar.titleTextAttributes = zeroAlphaAttributes
+            self.title = ""
+            
+            if let previousTitle = previousViewController.navigationItem.title {
+                previousViewController.setBackButtonTitle(title: previousTitle)
+            }
+            
+        case headerSecondQuarter:
+            navBar.titleTextAttributes = scalingAlphaAttributes
+            self.title = title
+            
+            previousViewController.setBackButtonTitle(title: "")
+        default:
+            navBar.titleTextAttributes = oneAlphaAttributes
+        }
+    }
+}
