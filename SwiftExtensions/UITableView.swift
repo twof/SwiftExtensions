e70d15a1985ac535cfbcd2ccaf98ee49dd3084ce
twof <fabiobean2@gmail.com>
2017-11-13 14:03:26 -0800

    New sectionheader extension

--- SwiftExtensions/UITableView.swift
+++ SwiftExtensions/UITableView.swift
@@ -1,9 +1,20 @@
 import UIKit
 
 extension UITableView {
-    var sectionHeaders: [UITableViewHeaderFooterView] {
+    var dequeuedSectionHeaders: [UITableViewHeaderFooterView] {
         return (0..<self.numberOfSections).map { (index) -> UITableViewHeaderFooterView? in
             return headerView(forSection: index)
+            }.flatMap { $0 }
+    }
+}
+
+extension UITableView {
+    var sectionHeaders: [UITableViewHeaderFooterView]? {
+        guard let deleg = self.delegate else { return nil }
+        return (0..<self.numberOfSections).map { (index) -> UITableViewHeaderFooterView? in
+            guard let view = deleg.tableView?(self, viewForHeaderInSection: index) as? UITableViewHeaderFooterView
+                else { return nil }
+            return view
             }.flatMap { $0 }
     }
 }
