2cf49c5ae292a55cc2f60a82cebb3bcd6ce7e55f
twof <fabiobean2@gmail.com>
2017-11-14 16:39:34 -0800

    Multiprefix check

--- SwiftExtensions/String.swift
+++ SwiftExtensions/String.swift
@@ -51,4 +51,15 @@ extension String {
     func tokenizeCommandline() -> [String] {
         return self.splitBefore(seperators: [" --", " -"])
     }
+
+    /// Returns true if a string has any one of a list of prefixes
+    /// Can be treated like a boolean OR
+    func hasPrefix(prefix: [String]) -> Bool{
+        for pre in prefix {
+            if self.contains(pre) {
+                return true
+            }
+        }
+        return false
+    }
 }
