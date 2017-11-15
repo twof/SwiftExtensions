01173b9691aa00f0c7ae6516535dff99df1064f6
twof <fabiobean2@gmail.com>
2017-11-14 16:38:01 -0800

    Added ArrayElementString

---
+++ SwiftExtensions/ArrayElementString.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+/// Takes CommandLine.arguments and groups commands/flags with their options
+extension Array where Element == String {
+    func group(by isNew:  (String) -> Bool) -> [(command: String, arguments: [String])]{
+        var grouped = [(String, [String])]()
+        var currentCommand = self[0]
+        var currentArgs = [String]()
+
+        for i in 1..<self.count {
+            if isNew(self[i]) {
+                grouped.append((currentCommand, currentArgs))
+                currentCommand = self[i]
+                currentArgs = []
+            } else {
+                currentArgs.append(self[i])
+            }
+        }
+
+        grouped.append((currentCommand, currentArgs))
+
+        return grouped
+    }
+}
